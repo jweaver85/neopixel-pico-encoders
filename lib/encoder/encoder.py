ca4ce24d06342a7e9ca1573ba32d800b583be9d1
jweaver <justinweaver1985@gmail.com>
2021-12-16 21:03:48 -0700

    fix linting error

--- lib/encoder/encoder.py
+++ lib/encoder/encoder.py
@@ -12,7 +12,8 @@ count = 0
 while True:
     position = encoder.position
     if position != last_position:
+        last_position = position
         # print(int(position))
     if button.value:
+        count += 1
         # print("button is pressed!")
-    last_position = position
